f00d4b4d9e8aa0cd9e514a300b562d30caec8299
kay <kayjem06@gmail.com>
2020-12-28 05:37:46 +0900

    20201228_page_1

--- board/src/main/java/com/board/dao/BoardDAO.java
+++ board/src/main/java/com/board/dao/BoardDAO.java
@@ -10,5 +10,8 @@ public interface BoardDAO {
 	public void write(BoardVO vo) throws Exception;
 	public BoardVO view(int bno) throws Exception;
 	public void modify(BoardVO vo) throws Exception;
-	public void delete(int bno) throws Exception;	
+	public void delete(int bno) throws Exception;
+	public int count() throws Exception;
+	public List listPage(int displayPost, int postNum) throws Exception;
+
 }
--- board/src/main/java/com/board/service/BoardServiceImpl.java
+++ board/src/main/java/com/board/service/BoardServiceImpl.java
@@ -42,5 +42,18 @@ public class BoardServiceImpl implements BoardService {
 	// 게시물 삭제
 	 public void delete(int bno) throws Exception {
 	  dao.delete(bno);
+	 }
+	 
+	// 게시물 총 갯수
+	 @Override
+	 public int count() throws Exception {
+	  return dao.count();
 	 }	 
+	 
+	// 게시물 목록 + 페이징
+	 @Override
+	 public List<?> listPage(int displayPost, int postNum) throws Exception {
+	  return dao.listPage(displayPost, postNum);
+	 }	 
+	 
 }
